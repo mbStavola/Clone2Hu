92aac5969d2997c6f7a27b13c0e3f5e887941ed8
mbs <mstavola9@gmail.com>
2014-05-25 01:05:59 -0400

    Initialized libgdx project

---
+++ desktop/src/com/basecolon/clone2hu/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.basecolon.clone2hu.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.basecolon.clone2hu.Clone2Hu;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Clone2Hu(), config);
+	}
+}
